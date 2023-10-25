58a32cf6bde42cecd8dbab1c60876fdfad8c2e55
Gage-Adams0 <148603428+Gage-Adams0@users.noreply.github.com>
2023-10-25 09:47:48 -0500

    Push Test

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String arg[]){
+        TestClass test = new TestClass();
+
+        test.testFunction(10);
+    }
+}
---
+++ TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+
+
+    public void testFunction(int num){
+        System.out.println(num * 2);
+    }
+}
