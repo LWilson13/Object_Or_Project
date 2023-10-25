855ba67d49b5894413abb8c1c5cc4a8e3b61b5ee
Gage-Adams0 <148603428+Gage-Adams0@users.noreply.github.com>
2023-10-25 16:58:11 -0500

    Added code to create a simple window with a button. Couldn't get it to run.

--- Main.java
+++ Main.java
@@ -1,9 +1,28 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Main extends Application{
 
-
-public class Main {
-    public static void main(String arg[]){
-        TestClass test = new TestClass();
-
-        test.testFunction(10);
+    Button button;
+    public static void main(String[] args){
+       launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Space Game");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene mainScene = new Scene(layout,750, 250);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
+    }
+
 }
